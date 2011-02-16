02de75af32ad444aa542879cee9c2665c7ed8e6b
bkurinsky <blake.kurinsky@gmail.com>
2011-02-15 18:55:49 -0600

    Cleaned some classes.

--- src/com/hello/HelloWorld.java
+++ src/com/hello/HelloWorld.java
@@ -1,11 +1,6 @@
 package com.hello;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 
 public class HelloWorld extends Activity {
