7dffabbb9e939937e1294d0aad492e0ab186dc58
tobias2143 <tobias2143@gmail.com>
2011-04-16 17:10:50 -0500

    -spinner selection now autofills first & last name

--- src/com/risotto/view/prescription/PrescriptionAdd.java
+++ src/com/risotto/view/prescription/PrescriptionAdd.java
@@ -1,7 +1,9 @@
 package com.risotto.view.prescription;
 
 import android.app.Activity;
+import android.content.ContentUris;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.risotto.R;
@@ -22,7 +25,8 @@ public class PrescriptionAdd extends Activity implements OnClickListener, OnItem
 	
 	private static final String[] PATIENT_PROJECTION = {
 		StorageProvider.PatientColumns._ID,
-		StorageProvider.PatientColumns.PATIENT_FIRST_NAME
+		StorageProvider.PatientColumns.PATIENT_FIRST_NAME,
+		StorageProvider.PatientColumns.PATIENT_LAST_NAME
 	};
 
 	@Override
@@ -43,18 +47,65 @@ public class PrescriptionAdd extends Activity implements OnClickListener, OnItem
 		  adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		  spinner.setAdapter(adapter);
 		  
+		  adapter.add(NEW_PATIENT);
+		  
 		  while(patientCursor.moveToNext()) {
-			  CharSequence name = patientCursor.getString(patientCursor.getColumnIndex(StorageProvider.PatientColumns.PATIENT_FIRST_NAME));
-			  Log.d(LOG_TAG,name.toString());
-			  adapter.add(name);
+			  CharSequence firstName = patientCursor.getString(patientCursor.getColumnIndex(StorageProvider.PatientColumns.PATIENT_FIRST_NAME)).trim();
+			  CharSequence lastName = patientCursor.getString(patientCursor.getColumnIndex(StorageProvider.PatientColumns.PATIENT_LAST_NAME)).trim();
+			  CharSequence fullName = firstName + " " + lastName;
+			  adapter.add(fullName);
 		  }
 		  
-		  adapter.add(NEW_PATIENT);		  
+		  spinner.setOnItemSelectedListener(this);
+		  patientCursor.close();
+		  
+		  EditText firstName = (EditText)findViewById(R.id.prescription_add_patient_first_name);
+		  firstName.setHint(R.string.prescription_add_patient_first_name_hint);
+		  
+		  EditText lastName = (EditText)findViewById(R.id.prescription_add_patient_last_name);
+		  lastName.setHint(R.string.prescription_add_patient_last_name_hint);
 	}
 	
-	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
-			long arg3) {
+	public void onItemSelected(AdapterView<?> parent, View view, int viewPosition, long rowId) {
 		// TODO Auto-generated method stub
+		Log.d(LOG_TAG, "in onItemSelected. ");
+		Log.d(LOG_TAG, "viewPosition: " + viewPosition);
+		Log.d(LOG_TAG, "rowId: " + rowId);
+		
+		if(0 == viewPosition) {
+			//new patient was selected
+		}
+		else {
+			//attempt to find patient in DB
+			
+			CharSequence fullName = (CharSequence)parent.getItemAtPosition(viewPosition);
+			
+			String[] name = ((String)fullName).split(" ");
+			String firstName = name[0].trim();
+			String lastName = name[1].trim();
+			
+			//Uri patientUri = ContentUris.withAppendedId(StorageProvider.PatientColumns.CONTENT_URI, rowId);
+			
+			String where = "patients.first_name='" + firstName + "' AND patients.last_name='" + lastName + "'";
+			
+			Cursor patientCursor = getContentResolver().query(
+					StorageProvider.PatientColumns.CONTENT_URI, 
+					PATIENT_PROJECTION, 
+					where, 
+					null, 
+					null);
+			
+			patientCursor.moveToFirst();
+			
+			EditText fName = (EditText) findViewById(R.id.prescription_add_patient_first_name);
+			fName.setText(patientCursor.getString(patientCursor.getColumnIndex(StorageProvider.PatientColumns.PATIENT_FIRST_NAME)));
+			fName.setEnabled(false);
+			
+			EditText lName = (EditText) findViewById(R.id.prescription_add_patient_last_name);
+			lName.setText(patientCursor.getString(patientCursor.getColumnIndex(StorageProvider.PatientColumns.PATIENT_LAST_NAME)));
+			lName.setEnabled(false);
+		}
+			
 
 	}
 
