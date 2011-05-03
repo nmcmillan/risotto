adb9d16e55a80af3c00ea06383b0f1bb4b4a9391
bkurinsky <blake.kurinsky@gmail.com>
2011-05-02 19:57:16 -0500

    Completed logic for scheduling - scheduled prescriptions will now display basic notifications. Removed an unused java class. TODO: Complete the necessary logic to populate notification details.

--- src/com/risotto/service/MainService.java
+++ src/com/risotto/service/MainService.java
@@ -3,7 +3,7 @@ package com.risotto.service;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Enumeration;
-import java.util.Vector;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -16,6 +16,7 @@ import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+
 import com.risotto.controller.StatusBarNotification;
 import com.risotto.controller.StatusBarNotificationManager;
 import com.risotto.model.Drug;
@@ -36,10 +37,12 @@ public class MainService extends Service {
 	public static final String ACTION_ALARM_TRIGGER = "com.risotto.service.ALARM_TRIGGER";
 	// The intent used to schedule an alarm.
 	public static final String ACTION_ALARM_SCHEDULE = "com.risotto.service.ALARM_SCHEDULE";
-	
-	private static boolean alarmScheduled = false;
-	
+	// The intent used when the schedules need to be recalculated.
+	public static final String ACTION_ALARM_RESCHEDULE = "com.risotto.service.ALARM_RESCHEDULE";
 
+	// The schedule id placed in the alarm pending intent
+	private static final String PENDING_INTENT_SCHEDULE_ID = "SCHEDULE_ID";
+	
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
@@ -65,11 +68,9 @@ public class MainService extends Service {
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		// If this is NOT the BOOT_COMPLETE or USER_PRESENT action...
-		if ( !(intent.getAction().equals(ACTION_START_SERVICE)) ) {
-			// ...send the intent to the handler.
-			handleCommand(intent);
-		}	
+		// Handle this command by passing it to the following method.
+		handleCommand(intent);
+		
 		return Service.START_STICKY;
 	}
 	
@@ -151,13 +152,12 @@ public class MainService extends Service {
 					Schedule newSchedule = new Schedule(prescription.get_id(), firstDate.getTimeInMillis(), Schedule.TWENTY_FOUR_HOURS_IN_MS );
 					Uri scheduleUri = this.getApplicationContext().getContentResolver().insert(StorageProvider.ScheduleColumns.CONTENT_URI, newSchedule.toContentValues());
 					
-					// Send the alarm to the alarm manager
-					Intent scheduleIntent = this.buildPrescriptionIntent(prescription.get_id(), Integer.parseInt(scheduleUri.getPathSegments().get(1)));
+					// Create a pending intent which will be sent when the alarm is done.
+					Intent scheduleIntent = this.buildPrescriptionIntent(Integer.parseInt(scheduleUri.getPathSegments().get(1)));
 					
 					// Get an instance of the AlarmManager
 					AlarmManager am = (AlarmManager)this.getApplicationContext().getSystemService(Context.ALARM_SERVICE);
 					
-					// Create a pending intent which will be sent when the alarm is done.
 					PendingIntent pendingIntent = PendingIntent.getService(this, 0, scheduleIntent, PendingIntent.FLAG_ONE_SHOT);
 					
 					// Have the AlarmManager schedule the alarm.
@@ -165,7 +165,11 @@ public class MainService extends Service {
 					Log.d(LOG_TAG, "Setting an alarm for: " + new Date(firstDate.getTimeInMillis()).toString() );
 					Log.d(LOG_TAG, "Repeating every 24 hours.");
 					
-					//am.set(AlarmManager.RTC_WAKEUP, Schedule.TWENTY_FOUR_HOURS_IN_MS, pendingIntent);
+					Log.d(LOG_TAG, "Current time in ms: " + System.currentTimeMillis());
+					Log.d(LOG_TAG, "Scheduled for: " + firstDate.getTimeInMillis());
+					Log.d(LOG_TAG, "Difference of: " + (firstDate.getTimeInMillis() - System.currentTimeMillis()) );
+					
+					am.set(AlarmManager.RTC_WAKEUP, firstDate.getTimeInMillis(), pendingIntent);
 					
 				}
 				
@@ -232,8 +236,8 @@ public class MainService extends Service {
 						Schedule newSchedule = new Schedule(prescription.get_id(), firstDate.getTimeInMillis(), Schedule.SEVEN_DAYS_IN_MS );
 						Uri scheduleUri = this.getApplicationContext().getContentResolver().insert(StorageProvider.ScheduleColumns.CONTENT_URI, newSchedule.toContentValues());
 						
-						// Send the alarm to the alarm manager
-						Intent scheduleIntent = this.buildPrescriptionIntent(prescription.get_id(), Integer.parseInt(scheduleUri.getPathSegments().get(1)));
+						// Create the intent that will be sent when the alarm triggers
+						Intent scheduleIntent = this.buildPrescriptionIntent(Integer.parseInt(scheduleUri.getPathSegments().get(1)));
 						
 						// Get an instance of the AlarmManager
 						AlarmManager am = (AlarmManager)this.getApplicationContext().getSystemService(Context.ALARM_SERVICE);
@@ -246,7 +250,7 @@ public class MainService extends Service {
 						Log.d(LOG_TAG, "Setting an alarm for: " + new Date(firstDate.getTimeInMillis()).toString() );
 						Log.d(LOG_TAG, "Repeating every 7 days.");
 						
-						//am.set(AlarmManager.RTC_WAKEUP, Schedule.SEVEN_DAYS_IN_MS, pendingIntent);
+						am.set(AlarmManager.RTC_WAKEUP, firstDate.getTimeInMillis(), pendingIntent);
 						
 					}			
 				}
@@ -258,32 +262,45 @@ public class MainService extends Service {
 		
 	}
 	
+	@SuppressWarnings("unused")
 	private void scheduleGivenDay() {
 		// TODO: Fill in the logic that was copy/pasted in the scheduleNewPrescription() method.
 	}
 	
-	private Intent buildPrescriptionIntent(int prescriptionId, int scheduleId) {
+	private Intent buildPrescriptionIntent(int scheduleId) {
 		
-		Log.d(LOG_TAG, "Building intent for prescription " + prescriptionId + " in schedule entry " + scheduleId );
+		Log.d(LOG_TAG, "Building intent for schedule entry " + scheduleId );
 		
 		// Create an intent that will launch when the trigger is done.
 		Intent newIntent = new Intent(ACTION_ALARM_TRIGGER);
 		
-		newIntent.putExtra("PRESCRIPTION_ID", prescriptionId);
-		newIntent.putExtra("SCHEDULE_ID", scheduleId);
+		newIntent.setAction(ACTION_ALARM_TRIGGER + ":" + scheduleId);
 		
+		newIntent.putExtra(MainService.PENDING_INTENT_SCHEDULE_ID, scheduleId);
 		newIntent.setClass(this, MainService.class);
 		
 		return newIntent;
 	}
 	
+	private boolean isActionAlarmTriggerIntent(String actionString) {
+		if ( (actionString.split(":")[0]).equals(ACTION_ALARM_TRIGGER) ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	@SuppressWarnings("unused")
 	private boolean isScheduleChanged() {
 		Log.d(LOG_TAG, "The schedule has changed for this prescription.");
+		// TODO: implement this function to compare a given prescription to the items stored in the schedule table
 		return false;
 	}
 	
+	@SuppressWarnings("unused")
 	private void updateScheduledPrescription() {
 		Log.d(LOG_TAG, "Updating the schedule information for the prescription...");
+		// TODO: implement this function instead of simply removing the items from the schedule table
 	}
 	
 	/**
@@ -334,28 +351,19 @@ public class MainService extends Service {
 				
 				if ( specificScheduleCursor != null && specificScheduleCursor.moveToFirst()) {
 					// There are entries for this prescription in the schedule table.
-					
-					// Remove them and schedule them as new!
-					
 					Log.d(LOG_TAG, "There are entries in the schedule table for prescription ID: " + prescriptionId );
 					
-					/* 3. Check to see if this prescription has changed from it's original scheduling. */
-					if ( this.isScheduleChanged() ) {
-						/* 4. Update any changed prescriptions */
-						Log.d(LOG_TAG, "The schedule for prescription ID: " + prescriptionId + " has changed!");
-						this.updateScheduledPrescription();
-					} else {
-						// The schedule for this prescription has not changed.
-						Log.d(LOG_TAG, "The schedule for prescription ID: " + prescriptionId  + " has not changed.");
-					}
+					/* 3. Remove any schedules for this prescription. */
+					int countRemoved = this.getApplicationContext().getContentResolver().delete(StorageProvider.ScheduleColumns.CONTENT_URI, scheduleWhereClause, null);
 					
-				} else {
-					/* 5. Schedule any 'new' prescriptions */
-					// There are no entries for this prescription in the schedule table.
-					Log.d(LOG_TAG, "There are no entries for prescription ID: " + prescriptionId + " in the schedule table.");
-					// Create a new schedule for this prescription.
-					this.scheduleNewPrescription(prescriptionId);
+					Log.d(LOG_TAG, "Removed " + countRemoved + " rows from the schedule table..." );	
 				}
+				
+				/* 4. Schedule the prescriptions */
+				// There are no entries for this prescription in the schedule table.
+				Log.d(LOG_TAG, "There are no entries for prescription ID: " + prescriptionId + " in the schedule table.");
+				// Create a new schedule for this prescription.
+				this.scheduleNewPrescription(prescriptionId);
 				
 				// Close the specific query for the schedules.
 				specificScheduleCursor.close();
@@ -367,54 +375,44 @@ public class MainService extends Service {
 		// Close the query for all the prescription ids
 		prescriptionCursor.close();
 
-		/* 6. Listen for any changes that may occur to the prescription URI table */
+		/* 5. Listen for any changes that may occur to the prescription URI table */
 		this.getApplicationContext().getContentResolver().registerContentObserver(StorageProvider.DrugColumns.CONTENT_URI, true, new PrescriptionContentObserver(new Handler()));
 	}
 	
-	
-	
-	
-	
-	private void scheduleAlarm() {
-		
-		// If we have not scheduled the alarm yet...
-		if ( ! this.isAlarmScheduled() ) {
-			// Get an instance of the AlarmManager
-			AlarmManager am = (AlarmManager)this.getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-			
-			// Create an intent that will launch when the trigger is done.
-			Intent newIntent = new Intent(ACTION_ALARM_TRIGGER);
-			
-			newIntent.putExtra("TOP_TEXT", "Tylenol");
-			newIntent.putExtra("TITLE_TEXT", "Time to take 3 tylenol.");
-			newIntent.putExtra("BODY_TEXT", "Drink a full glass of water.");
-			
-			newIntent.setClass(this, MainService.class);
-			
-			// Create a pending intent which will be sent when the alarm is done.
-			PendingIntent pi = PendingIntent.getService(this, 0, newIntent, PendingIntent.FLAG_ONE_SHOT);
-			
-			// Have the AlarmManager schedule the alarm.
-			am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 10000, pi );
-			
-			// Set the alarm flag to true, as we have scheduled it
-			alarmScheduled = true;
-			
-		} else {
-			Log.d(LOG_TAG, "No need to schedule an alarm, already done once...");
-		}
-		
-	}
-	
-	private void notifyAlarmDone(Intent intent) {
+	private void displayAlarmNotification(Intent intent) {
 		// Get an instance of the SBNM
 		StatusBarNotificationManager sbnm = new StatusBarNotificationManager(this.getApplicationContext());
 		
-		// Create a new prescription.
-		Prescription prep = new Prescription(new Patient("Nick", "McMillan", Patient.GENDER.MALE),new Drug("Ibuprofen", Drug.TYPE.PRESCRIPTION, 400, "mg"),Prescription.DOSE_TYPE_EVERY_DAY,10,10);
+		// Get the schedule id from the intent.
+		int scheduleId = intent.getIntExtra(PENDING_INTENT_SCHEDULE_ID, -1);
 		
-		// Create the new status bar notification.
-		StatusBarNotification not = new StatusBarNotification(this, prep, intent.getStringExtra("TOP_TEXT"), intent.getStringExtra("TITLE_TEXT"), intent.getStringExtra("BODY_TEXT"));
+		if( scheduleId == -1) {
+			// Something didn't get bundled correctly, how do we handle this case?
+			return;
+		}
+		
+		// Get the schedule that triggered
+		Uri scheduleUri = ContentUris.withAppendedId(StorageProvider.ScheduleColumns.CONTENT_URI, scheduleId);
+		String[] scheduleProjection = { StorageProvider.ScheduleColumns._ID, StorageProvider.ScheduleColumns.SCHEDULES_PRESCRIPTION };
+		Cursor scheduleCursor = this.getApplicationContext().getContentResolver().query(scheduleUri, scheduleProjection, null, null, null);
+		scheduleCursor.moveToFirst();
+		
+		// Get the prescription associated with this alarm
+		Uri prescriptionUri = ContentUris.withAppendedId(StorageProvider.PrescriptionColumns.CONTENT_URI, scheduleCursor.getInt(scheduleCursor.getColumnIndex(StorageProvider.ScheduleColumns.SCHEDULES_PRESCRIPTION)));
+		Cursor prescriptionCursor = this.getApplicationContext().getContentResolver().query(prescriptionUri, null, null, null, null);
+		prescriptionCursor.moveToFirst();
+		
+		Log.d(LOG_TAG, "Notification for prescription " + scheduleCursor.getInt(scheduleCursor.getColumnIndex(StorageProvider.ScheduleColumns.SCHEDULES_PRESCRIPTION)) + " in schedule " + scheduleId);
+		
+		// Parse that cursor
+		Prescription prescription = Prescription.fromCursor(prescriptionCursor, this.getApplicationContext());
+		
+		// Create and display the notification
+		String topText = prescription.getDrug().getBrandName();
+		String titleText = "Please take X " + prescription.getDrug().getBrandName();
+		String bodyText = "Body text.";
+		
+		StatusBarNotification not = new StatusBarNotification(this, prescription, topText, titleText, bodyText);
 		
 		// Get the index of this notification
 		int x = sbnm.add(not);
@@ -426,11 +424,9 @@ public class MainService extends Service {
 			Log.d(LOG_TAG, "THERE WAS AN EXCEPTION!!");
 		}
 		
-		Log.d(LOG_TAG, "ALARM UP!!!!!");
-	}
-	
-	private boolean isAlarmScheduled() {
-		return alarmScheduled;
+		// Close the cursors
+		scheduleCursor.close();
+		prescriptionCursor.close();
 	}
 	
 	/**
@@ -444,78 +440,22 @@ public class MainService extends Service {
 		// Get the string action from the intent object.
 		String intentAction = intent.getAction();
 		
-		if ( intentAction.equals(ACTION_ALARM_TRIGGER) ) {
+		if ( this.isActionAlarmTriggerIntent(intentAction) ) {
 			// An alarm has completed!
-			notifyAlarmDone(intent);
-		} else if ( intentAction.equals(ACTION_ALARM_SCHEDULE) ) {
-			// Schedule the alarm...
-			//this.scheduleAlarm();
-			//this.scheduleTests();
+			displayAlarmNotification(intent);
+		} else if ( intentAction.equals(ACTION_START_SERVICE) || intentAction.equals(ACTION_ALARM_SCHEDULE) || intent.getAction().equals(ACTION_ALARM_RESCHEDULE) ) {
+			// Schedule alarms...
 			this.schedulePrescriptions();
 		} else {
 			// How did we get this action? What is this!?
 			Log.d(LOG_TAG, "handleCommand() - Unknown intent: " + intentAction);
-		}	
-		
+		}			
 	}
-	
-	private void scheduleTests() {
-		// Create a where clause to only get the prescriptions that are scheduled
-		String whereClause = StorageProvider.PrescriptionColumns.PRESCRIPTION_SCHEDULED + "=" + "'" + Prescription.SCHEDULED + "'";
-		
-		Log.d(LOG_TAG, "Getting a cursor on the scheduled prescriptions...");
-		
-		// Get all of the prescriptions that are scheduled
-		Cursor prescCursor = this.getApplicationContext().getContentResolver().query(StorageProvider.PrescriptionColumns.CONTENT_URI, null, whereClause, null, null);
-		
-		Log.d(LOG_TAG, "Got a cursor!");
-		
-		// Let's schedule some things...
-		
-		// Are there any prescription objects?
-		if (prescCursor.moveToFirst()) {
-			do {
-				Log.d(LOG_TAG, "DOING!");
-				
-				// Get the list of valid day columns for this prescription
-				Vector<String> validDayColumns = Prescription.getScheduledDayColumns(prescCursor);
-				
-				Enumeration<String> daysEnum = validDayColumns.elements();
-				
-				while(daysEnum.hasMoreElements()) {
-					// Get the valid column name
-					String dayColumnName = daysEnum.nextElement();
-					Log.d(LOG_TAG, "Getting the data at the column: " + dayColumnName);
-					Log.d(LOG_TAG, "For Patient: " + prescCursor.getString(prescCursor.getColumnIndex(StorageProvider.PrescriptionColumns.PRESCRIPTION_PATIENT)));
-					Log.d(LOG_TAG, "For Drug: " + prescCursor.getString(prescCursor.getColumnIndex(StorageProvider.PrescriptionColumns.PRESCRIPTION_DRUG)));
-
-					// Schedule a timer based on the data in the day column
-					//String dataValue = prescCursor.getString(prescCursor.getColumnIndex(dayColumnName));
-					
-					//Log.d(LOG_TAG, "Data @ " + dayColumnName + " : " + dataValue);
-					
-				}
-				
-				
-			} while (prescCursor.moveToNext());
-			
-			// Close and release the cursor.
-			prescCursor.close();
-			
-			// Register for any possible changes to this URI
-			this.getApplicationContext().getContentResolver().registerContentObserver(StorageProvider.DrugColumns.CONTENT_URI, true, new PrescriptionContentObserver(new Handler()));
-			
-		}
-		
-	}
-	
-	
 	
 	class PrescriptionContentObserver extends ContentObserver {
 
 		public PrescriptionContentObserver(Handler handler) {
-			super(handler);
-			
+			super(handler);	
 		}
 
 		@Override
@@ -524,6 +464,5 @@ public class MainService extends Service {
 			Log.d(LOG_TAG, "Updating schedules.");
 			MainService.this.schedulePrescriptions();
 		}
-
 	}
 }
